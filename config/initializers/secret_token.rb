eba085c7da519b8ea5aa1b97ecaf4877689becc1
Kimberly M <kcm230@nyu.edu>
2013-03-12 17:46:33 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kmrails::Application.config.secret_token = 'bbd326b1f1cb31c2864b3e817f9ba39507f500671565c282031dc8399c33b2b6ca22bd0cd338bfc31cdcc8f6e4a917d9df7be13437755d48a081e57e349d311d'
