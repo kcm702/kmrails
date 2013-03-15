fb6de79e3a50887d20ac8b5571a959762fefadd8
Kimberly M <kcm230@nyu.edu>
2013-03-15 00:00:51 -0400

    Added devise and simple form gems

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Kmrails::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Required for Devise gem
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
